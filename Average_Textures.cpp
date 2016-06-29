18ca8a08e05a9bc5f582c1975177bb748cfbdc6f
sahlet@yandex.ru <11122aq>
2016-06-29 17:02:29 +0300

    not bad
    :)

--- Average_Textures.cpp
+++ Average_Textures.cpp
@@ -7,16 +7,6 @@
 #include <algorithm>
 //#include <typeinfo>
 
-
-matrix< array<pair<T, bool>, 3> > get_textures_arrangement(const matrix<T>& source) throw(exception){
-	T w = source.get_w(), h = source.get_h();
-	if (w < 2 || h < 2){
-		//тривиально
-		//сделать
-		matrix< array<pair<T, bool>, 3> > sdelay_menya_normalno(w, h);
-		return sdelay_menya_normalno;
-	}
-
 struct node{
 	T x, y;
 	node(const T& x = 0, const T& y = 0) : x(x), y(y){}
@@ -45,43 +35,56 @@ struct node{
 	}
 };
 
+enum limits {NOTHING = 0, UP = 1, DOWN = 2, LEFT = 4, RIGHT = 8};
+list< node > make_neighbors(const node& n, const T& w, const T& h, limits lim = NOTHING){
+	T x = n.x, y = n.y;
+	limits limitless = (limits)((limits)(
+			(y > 0) | 
+			((y + 1 < h) << 1) | 
+			((x > 0) << 2) | 
+			((x + 1 < w) << 3) ) ^ lim);
+
+	list< node > neighbors;
+	if (limitless & limits::DOWN){
+		neighbors.push_back(node(x, y + 1));
+		if (limitless & limits::RIGHT) neighbors.push_back(node(x + 1, y + 1));
+		if (limitless & limits::LEFT) neighbors.push_back(node(x - 1, y + 1));
+	}
+	if (limitless & limits::RIGHT) neighbors.push_back(node(x + 1, y));
+	if (limitless & limits::LEFT) neighbors.push_back(node(x - 1, y));
+	if (limitless & limits::UP){
+		neighbors.push_back(node(x, y - 1));
+		if (limitless & limits::RIGHT) neighbors.push_back(node(x + 1, y - 1));
+		if (limitless & limits::LEFT) neighbors.push_back(node(x - 1, y - 1));
+	}
+	
+	return neighbors;
+}
+
 
 class info{//информация про узел
 public:
 	enum level {NONE = 0, FIRST = 1, SECOND = 2, THIRD = 4};
 private:
-	int used;//использованные (этим узлом) уровни
-	int taken_before, taken_after;//уровни взятые кем-то (не этим узлом) до выбора в этом узле и после.
+	int used;//использованные уровни
+	int taken_before; //уровни, взятые в предыдущих соседних узлах.
+	int taken_after;//уровень, взятый в следующем узле.
 	/*в taken_after будем помещать только уровень, который взял один следующий узел, а в taken_before все возможные (в соответствии с его описанием см.выше.)*/
 
 	int cur_level;//показывает на каком уровне сейчас стоит 1
 	bool can_be_used_again_;
-	array< T , 3> textures;
+	array< T , 3 > textures;//(значимые только те текстуры, которые отмечены в taken_before или в taken_after или в cur_level)
 public:
-	info() : used(NONE), taken_before(NONE), taken_after(NONE), cur_level(NONE){}
+	info() : used(NONE), taken_before(NONE), taken_after(NONE), cur_level(NONE), can_be_used_again_(true){}
 	
-	//кто-то хочет записать текстуру на уровень.
 	inline bool take(const level& l, const T& texture){
-		if (cur_level) throw(exception("после отладки программы этот throw надо удалить"));
+		//следующий узел хочет записать текстуру texture на уровень l и вызывает эту функцию.
+		if (!cur_level) throw(exception("после отладки программы этот throw надо удалить"));
 		if (l & (taken_before | cur_level)){
 			if (textures[l/2] != texture) return false/*нельзя добавлять текстуру*/;
 		} else {
 			if(!l) throw(exception("после отладки программы этот throw надо удалить"));
-			if (cur_level) taken_after = l;
-			else taken_before |= l;
-			textures[l/2] = texture;
-		}
-		return true;
-	}
-
-	inline bool take(const level& l, const T& texture){
-		if (cur_level) throw(exception("после отладки программы этот throw надо удалить"));
-		if (l & (taken_before | cur_level)){
-			if (textures[l/2] != texture) return false/*нельзя добавлять текстуру*/;
-		} else {
-			if(!l) throw(exception("после отладки программы этот throw надо удалить"));
-			if (cur_level) taken_after = l;
-			else taken_before |= l;
+			taken_after = l;
 			textures[l/2] = texture;
 		}
 		return true;
@@ -89,21 +92,74 @@ public:
 	
 	//текстура должна стать на любой свободный уровень, который еще не использован
 	inline void use(const T& texture, const node& n, const matrix< info >& infos) throw(exception){
-		auto tmp = (FIRST | SECOND | THIRD) ^ (taken_before | used);
-		if (used){
-		}
-	}
+		if (!can_be_used_again_) throw(exception("после отладки программы этот throw надо удалить"));
+		if (!used){//первый раз выставляем уровень - значит надо определить taken_before
+			limits limitless = (limits)(
+			(n.y > 0) | 
+			((n.y + 1 < infos.get_h()) << 1) | 
+			((n.x > 0) << 2));
 
+			list< node > neighbors;
+			if (limitless & (limits::DOWN | limits::LEFT)) neighbors.push_back(node(n.x - 1, n.y + 1));
+			if (limitless & limits::LEFT) neighbors.push_back(node(n.x - 1, n.y));
+			if (limitless & limits::UP){
+				neighbors.push_back(node(n.x, n.y - 1));
+				if (limitless & limits::LEFT) neighbors.push_back(node(n.x - 1, n.y - 1));
+			}
+
+			int l;
+			for (const auto& i : neighbors){
+				l = infos(i.x, i.y).cur_level;
+				if (l & taken_before){
+					//этот код, как мне кажется, не должен никогда выполниться
+					//if (textures[l/2] != infos(i.x, i.y).textures[l/2]) throw(exception(/*нельзя добавлять текстуру (этот throw не надо удалять)*/));
+					if (textures[l/2] != infos(i.x, i.y).textures[l/2]) throw(exception("после отладки программы этот throw надо удалить"));
+				} else {
+					taken_before |= l;
+					textures[l/2] = infos(i.x, i.y).textures[l/2];
+				}
+			}
+		}
+
+		if (FIRST ^ used && FIRST & taken_before && textures[0] == texture) cur_level = FIRST;
+		else if(SECOND ^ used && SECOND & taken_before && textures[1] == texture) cur_level = SECOND;
+		else if(THIRD ^ used && THIRD & taken_before && textures[2] == texture) cur_level = THIRD;
+		else if(FIRST ^ used && FIRST ^ taken_before) cur_level = FIRST;
+		else if(SECOND ^ used && SECOND ^ taken_before) cur_level = SECOND;
+		else if(THIRD ^ used && THIRD ^ taken_before) cur_level = THIRD;
+		else throw(exception(/*нельзя добавлять текстуру - либо все варианты перебраны, либо такую текстуру никогда нельзя вставить (этот throw не надо удалять)*/));
+		
+		used |= cur_level;
+
+		if ((FIRST | SECOND | THIRD) ^ (taken_before | used)){//какой-то уровень остался и его можно было бы использовать при другой расстановке
+			can_be_used_again_ = true;
+		} else can_be_used_again_ = false;
+	}
 	bool can_be_used_again(){
 		if (!used) throw(exception("после отладки программы этот throw надо удалить"));
-		return can_be_used_again;
+		return can_be_used_again_;
 	}
 
 	inline T get_texture() throw(exception){
 		if (!cur_level) throw(exception("текстура в узле не определена"));
 		return textures[cur_level/2];
 	}
+	void clear(){
+		used = NONE; taken_before = NONE; taken_after = NONE; cur_level = NONE; can_be_used_again_ = true;
+	}
 };
+
+
+matrix< array<pair<T, bool>, 3> > get_textures_arrangement(const matrix<T>& source) throw(exception){
+	T w = source.get_w(), h = source.get_h();
+	if (w < 2 || h < 2){
+		//тривиально
+		//сделать
+		matrix< array<pair<T, bool>, 3> > sdelay_menya_normalno(w, h);
+		return sdelay_menya_normalno;
+	}
+
+
 
 	enum number_of_free_columns : int {ONE, TWO, THREE} num_of_columns = ONE;
 	auto changeable_source = source;
